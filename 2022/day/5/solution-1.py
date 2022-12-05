388a5e8b3cd115fdfa65b795fe59d58f736f3154
cacauisadog <cbcaue@protonmail.com>
2022-12-05 09:11:40 -0300

    submits solution to 2022's day 5, in python

---
+++ 2022/day/5/solution-1.py
@@ -0,0 +1,36 @@
+stacks = {
+    1: "BPNQHDRT",
+    2: "WGBJTV",
+    3: "NRHDSVMQ",
+    4: "PZNMC",
+    5: "DZB",
+    6: "VCWZ",
+    7: "GZNCVQLS",
+    8: "LGJMDNV",
+    9: "TPMFZCG"
+}
+
+def _move_stack(_qty, _from, _to):
+    to_move = stacks[_from][-_qty:]
+    stacks[_from] = stacks[_from][:-_qty]
+    stacks[_to] += to_move[::-1]
+
+def _print_result():
+    result = ""
+    for stack in stacks:
+        result += stacks[stack][-1:]
+
+    print(result)
+
+
+with open("input.txt", "r") as input:
+    for line in input:
+        if line.startswith("move"):
+            instructions = line.split(" ")
+            _qty = int(instructions[1])
+            _from = int(instructions[3])
+            _to = int(instructions[5])
+
+            _move_stack(_qty, _from, _to)
+
+_print_result()  # ZBDRNPMVH
---
+++ 2022/day/5/thoughts.md
@@ -0,0 +1,1 @@
+I absolutely f****** _hate_ Hanoi's tower.
