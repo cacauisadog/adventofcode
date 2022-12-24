e2b9e0b27c7ff93f75aa3bf2e43c42ed8d36333a
Cauê Bittencourt <cbcaue@protonmail.com>
2022-12-24 20:49:23 -0300

    skeleton of a solution to day 9

---
+++ 2022/day/9/solution-1.py
@@ -0,0 +1,22 @@
+class RopeBridge():
+    def __init__(self):
+        self.grid = [['H']]
+
+    def print_grid(self):
+        for row in self.grid:
+           print(" ".join(map(str, row)))
+
+    def move_up(self, times):
+        pass
+
+    def move_right(self, times):
+        pass
+
+    def move_down(self, times):
+        pass
+
+    def move_left(self, times):
+        pass
+
+    def _head_and_tail_are_adjacent(self):
+        pass
